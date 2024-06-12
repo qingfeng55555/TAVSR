bf3257e1803dd65e50f9eedb76c03c0e86a17fde
qingfeng55555 <50541145+qingfeng55555@users.noreply.github.com>
2024-06-12 11:36:58 +0800

    Delete RUF_fusion_exp directory

--- RUF_fusion_exp/dataset/__init__.py
+++
@@ -1,7 +0,0 @@
-from .vimeo90k import Vimeo90KDataset
-from .mfqev2 import MFQEv2Dataset, VideoTestMFQEv2Dataset
-
-__all__ = [
-    'Vimeo90KDataset',
-    'MFQEv2Dataset', 'VideoTestMFQEv2Dataset', 
-    ]
